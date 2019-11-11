60a8e8a43efd16c89e333c3cb0682a45f387998d
Scubaboy <robin@robincawsey.plus.com>
2019-11-11 22:36:36 +0000

    initial

--- sketch_nov11a/sketch_nov11a.ino
+++ sketch_nov11a/sketch_nov11a.ino
@@ -1,8 +1,16 @@
 #include <EEPROM.h>
 #include "Types.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{  
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  Serial.println("Configure state controller eeprom config.");
+  
+  // Format eeprom with state controller config
   SystemState::State state;
 
   state.LastRetractPosition = RetractTypes::UnDefined;
@@ -11,11 +19,29 @@ void setup() {
   for (int iter = 0; iter < 6; iter++)
   {
     state.servoAttributes[iter].MaxAngle = 180;
-    state.servoAttributes[iter].MinAngle = 180;
+    state.servoAttributes[iter].MinAngle = 0;
     state.servoAttributes[iter].DegPerSecond = 1;
   }
   
   EEPROM.put(0, state);
+
+  //check eeprom write
+  SystemState::State readState;
+
+  Serial.println("Read state controller eeprom config.");
+  EEPROM.get(0, readState);
+  Serial.println(readState.Version);
+  Serial.println(readState.LastRetractPosition);
+  
+  Serial.println("Read state controller servo eeprom config.");
+  for (int iter = 0; iter < 6; iter++)
+  {
+    Serial.println(readState.servoAttributes[iter].MaxAngle);
+    Serial.println(readState.servoAttributes[iter].MinAngle);
+    Serial.println(readState.servoAttributes[iter].DegPerSecond);
+  }
+
+  
   
 }
 
